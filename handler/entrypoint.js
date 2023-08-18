1144546b8af2eda3165503345d2b06fb5b91d779
dadduedo <dadduedo@gmail.com>
2023-08-18 16:22:43 +0200

    sistemato bug per il primo inserimento, sistemato cloudfrontinvalidation

--- handler/entrypoint.js
+++ handler/entrypoint.js
@@ -1,62 +1,73 @@
 const s3Upload  = require('../services/helper/s3Upload');
 const cloudFrontInvalidation = require('../services/helper/cloudFrontInvalidation');
 const mysql = require('serverless-mysql')(process.env.MYSQL_DSN);
+const fs = require('fs');
+const path = require('path');
 
 module.exports = async (event) => {
-    let results = await mysql.query('SELECT zip_code,town,province,region,city FROM area order by province');
+    const results = await mysql.query('SELECT zip_code, town, province, region, city FROM area ORDER BY province');
     await mysql.end();
     mysql.quit();
-    var jsontosend = {}
-    var jsonRegion = {}
-    var jsonProvinceReg = {}
-    var jsonProvince = {}
-    var lastProv = ""
-    var arrayRegions = []
-    if (!results.length) {
-        return 'No results were found in table';
-    }
-    for (var i=0;i<results.length;i++){
-        //creazione province
-        jsonProvince[results[i].province] = results[i].city
-        //creazione regioni
-        jsonRegion["name"] = results[i].region
-        //ciclo di nuovo sui risultati della query perchè non sono in ordine di regione
-        for (var j=0;j<results.length;j++){
-            if(results[i].region == results[j].region){
-                jsonProvinceReg[results[j].province] = results[j].city
-            }
 
+    const provincesByRegion = {};
+    let jsontosend = {};
+    const arrayRegions = [];
+    const jsonProvinces = {}
+    let currentProvince = "";
+
+    for (const result of results) {        
+        // Verifica cambiamento di provincia
+        if (currentProvince !== result.province) {
+            await s3Upload(JSON.stringify(jsontosend), currentProvince);
+            currentProvince = result.province;
+            jsontosend = {};
         }
-        jsonRegion["provinces"] = jsonProvinceReg
-        jsonProvinceReg = {}
-        arrayRegions.push(jsonRegion)
-        jsonRegion = {}
-        //creazione tutti i file di tutte le province
-        if (results[i+1] == undefined) {
-           jsontosend[results[i].zip_code] = "" + results[i].zip_code + " - " + results[i].town + ""
-           lastProv = results[i].province
-       }
-       else{
-           if (results[i].province == results[i+1].province) {
-               jsontosend[results[i].zip_code] = "" + results[i].zip_code + " - " + results[i].town + ""
-           } else {
-               await s3Upload(JSON.stringify(jsontosend), results[i].province);
-               jsontosend = {}
-           }
-       }
+        // creazione del json per ogni provincia
+        jsontosend[result.zip_code] = `${result.zip_code} - ${result.town}`;
+
+        // Creazione regioni 
+        if (!provincesByRegion[result.region]) {
+            provincesByRegion[result.region] = {};
+        }
+        //preparazione json da ciclare sul socondo for 
+        provincesByRegion[result.region][result.province] = result.city;
+
+        // Creazioni province
+        jsonProvinces[result.province] = result.city;
+        
+    }
+    //if che serve per creare l'ultima provincia perchè il primo ciclo non può coprirla
+    if (Object.keys(jsontosend).length > 0) {
+        await s3Upload(JSON.stringify(jsontosend), currentProvince);
     }
 
+    for (const regionName in provincesByRegion) {
+        const region = {
+            name: regionName,
+            provinces: provincesByRegion[regionName]
+        };
+        arrayRegions.push(region);
+    }
+    await s3Upload(JSON.stringify(arrayRegions), "regions");
+    await s3Upload(JSON.stringify(jsonProvinces), "provinces")
+    ;
+    
     if (process.env.STAGE !== 'dev') {
-        await cloudFrontInvalidation();
+        const folderPath = 'data/buckets/zipcode-enabled';
+        fs.readdir(folderPath, async (err, files) => {
+            if (err) {
+                console.error('Errore durante la lettura della cartella:', err);
+                return;
+            }
+            // Filtra i file (ignorando le sottocartelle)
+            var fileList = files.filter(file => fs.statSync(path.join(folderPath, file)).isFile());
+            
+            await cloudFrontInvalidation(fileList);
+        });
+       
     }
-    //rende unico il file delle regioni
-    const uniqueArrRegions = arrayRegions.filter((thing, index, self) =>
-            index === self.findIndex((t) => t.name === thing.name)
-    );
-    // scrive i file nella cartella buckets
-    await s3Upload(JSON.stringify(jsonProvince), "provinces")
-    await s3Upload(JSON.stringify(uniqueArrRegions), "regions")
-    await s3Upload(JSON.stringify(jsontosend), lastProv)
+    
+    
 
-    return "Create a file of provinces, Create a file of regions, Create a file for each province"
+    return "Created files for provinces and regions";
 };
